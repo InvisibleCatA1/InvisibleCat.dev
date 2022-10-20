2e250a3008b3923adc4e05aad84f4fd9ebe2f282
Coen Shields <shieldscoen@gmail.com>
2022-10-20 15:06:28 -0600

    doing it all over again (part 1)

---
+++ components/data/projects.js
@@ -0,0 +1,26 @@
+const projects = [
+    {
+        id: "01",
+        title: "Fairview Robotics",
+        description: "FRC team 2036",
+        img: "/public/assets/images/robotics.png",
+        category: "Projects",
+        keyword: ["Project", "Robotics"]
+    },
+    {
+        id: "02",
+        title: "Reload",
+        description: "Reload is a minecraft hack client",
+        img: "/public/assets/images/reload.png",
+        category: "Projects",
+        keyword: ["Project", "Reolad", "Hack", "Client"]
+    },
+    {
+        id: "03",
+        title: "Learning Web Design",
+        description: "I am learning web desgin with Next.js (this website is made with it!)",
+        img: "/public/assets/images/web.png",
+        category: "Projects",
+        keyword: ["Project", "Programming"]
+    }
+]
